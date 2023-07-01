89f885162ad2175c7884a8772d7fa1ee84c26038
Minh Nguyen <minhh2792.work@gmail.com>
2023-07-01 23:52:06 +0700

    api key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://movieweb.minhh2792-work.workers.dev",
-  VITE_TMDB_READ_API_KEY: "46415a9e04902c816fe11138c21e3ce3"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjQxNWE5ZTA0OTAyYzgxNmZlMTExMzhjMjFlM2NlMyIsInN1YiI6IjY0YTA1N2Q1NGE1MmY4MDBjOTk0NWRkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DRLCqhHpPESRAEUTVqLil9p4AAuUQ1OtMhSbHPaVW4o"
 };
