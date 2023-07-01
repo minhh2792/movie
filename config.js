c34c32a5f3da4f9de654236bdeabbd1c0efd4fec
Minh Nguyen <minhh2792.work@gmail.com>
2023-07-01 23:50:51 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movieweb.minhh2792-work.workers.dev",
+  VITE_TMDB_READ_API_KEY: "46415a9e04902c816fe11138c21e3ce3"
+};
